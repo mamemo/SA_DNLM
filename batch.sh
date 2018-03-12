cc2f9478656351742dec7bd8ff8274a79d7d4ae3
Ubuntu <jdnlm@jdnlm.jdnlm1176.j7.internal.cloudapp.net>
2018-03-12 16:10:53 +0000

    Corridas Listas

--- batch.sh
+++ batch.sh
@@ -1,29 +1,3 @@
 java -Djava.library.path=x64 -jar sa.jar 1 0.38999999999999946 >> output.txt
 sleep 30
 
-java -Djava.library.path=x64 -jar sa.jar 1 0.38999999999999946 >> output.txt
-sleep 30
-
-java -Djava.library.path=x64 -jar sa.jar 1 0.38999999999999946 >> output.txt
-sleep 30
-
-java -Djava.library.path=x64 -jar sa.jar 1 0.38999999999999946 >> output.txt
-sleep 30
-
-java -Djava.library.path=x64 -jar sa.jar 1 0.38999999999999946 >> output.txt
-sleep 30
-
-java -Djava.library.path=x64 -jar sa.jar 1 0.38999999999999946 >> output.txt
-sleep 30
-
-java -Djava.library.path=x64 -jar sa.jar 1 0.38999999999999946 >> output.txt
-sleep 30
-
-java -Djava.library.path=x64 -jar sa.jar 1 0.38999999999999946 >> output.txt
-sleep 30
-
-java -Djava.library.path=x64 -jar sa.jar 1 0.38999999999999946 >> output.txt
-sleep 30
-
-java -Djava.library.path=x64 -jar sa.jar 1 0.38999999999999946 >> output.txt
-sleep 30
